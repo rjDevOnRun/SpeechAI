ae91bfa3d10dc4bb633d83a0c1bfbdb2c1ae3d26
rjDevOnRun <34760817+rjDevOnRun@users.noreply.github.com>
2018-09-08 19:11:06 +0900

    Add files via upload

---
+++ SpeechBOT/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace SpeechBOT
+{
+    partial class frmSpeechBOT
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Consolas", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkViolet;
+            this.label1.Location = new System.Drawing.Point(46, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(330, 47);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "A talking BOT";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // frmSpeechBOT
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(422, 109);
+            this.Controls.Add(this.label1);
+            this.Name = "frmSpeechBOT";
+            this.Text = "Speech BOT (Talk and Respond)";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ SpeechBOT/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.Speech.Recognition;
+
+
+namespace SpeechBOT
+{
+    public partial class frmSpeechBOT : Form
+    {
+        SpeechSynthesizer SPKE = new SpeechSynthesizer();
+        Choices list = new Choices();
+
+
+        public frmSpeechBOT()
+        {
+            IntroduceTheBOT();
+
+            TalkToMe();
+
+
+            InitializeComponent();
+        }
+
+        private void TalkToMe()
+        {
+            SpeechRecognitionEngine SRE = new SpeechRecognitionEngine();
+            list.Add(new string[] {"hello",
+                                    "how are you",
+                                    "nice to meet you",
+                                    "what is the time", "can you tell me the date", "please sleep" });
+
+            Grammar gr = new Grammar(new GrammarBuilder(list));
+
+            try
+            {
+                SRE.RequestRecognizerUpdate();
+                SRE.LoadGrammar(gr);
+                SRE.SetInputToDefaultAudioDevice();
+                SRE.RecognizeAsync(RecognizeMode.Multiple);
+                SRE.SpeechRecognized += sphRec_SpeechRecogonized;
+            }
+            catch
+            { return; }
+
+        }
+
+        private void sphRec_SpeechRecogonized(object sender, SpeechRecognizedEventArgs e)
+        {
+            string str = e.Result.Text;
+
+            if (str == "hello")
+            {
+                ReplyToMe("Hi!");
+            }
+            else if (str == "how are you")
+            {
+                ReplyToMe("Awesome!, how about you?");
+            }
+            else if (str == "nice to meet you")
+            {
+                ReplyToMe("Glad to be here for you!");
+            }
+            else if (str == "what is the time")
+            {
+                ReplyToMe("The time is " + System.DateTime.Now.ToString("h:mm tt"));
+            }
+            else if (str == "can you tell me the date")
+            {
+                ReplyToMe("The date is " + System.DateTime.Now.ToString("M/d/yyyy"));
+            }
+            else if (str == "please sleep")
+            {
+                ReplyToMe("OK!, Catch you later!");
+                Application.Exit();
+            }
+            else
+            {
+                return;
+            }
+
+        }
+
+        private void ReplyToMe(string s)
+        {
+            SPKE.Speak(s);
+        }
+
+        private void IntroduceTheBOT()
+        {
+            SPKE.SelectVoiceByHints((VoiceGender.Female));
+            SPKE.Speak("Hello, I am your assistant. How are you today?");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
